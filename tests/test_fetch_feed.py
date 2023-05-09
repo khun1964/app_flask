0e2e80f4c17902607da2d555a3b0eb7119402d19
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2023-05-09 16:39:00 +0200

    Added some comments explaining the role of the test

--- tests/test_fetch_feed.py
+++ tests/test_fetch_feed.py
@@ -2,7 +2,9 @@ import main
 
 def test_working_feed():
     """
-    A dummy test, used to make sure we can call pytest
+    The only business method we have for now is the feed reader one.
+    As a consequence, the only valid test is the one getting data from a working feed
+    and checking there are feed entries in.
     """
     feed_entries = main.fetch_feed("https://stackoverflow.com/feeds")
     assert len(feed_entries)>0
