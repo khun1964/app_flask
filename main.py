a7d4be68703a27c4b4c8e97011926d3a1a59edfe
hayetyefsah <122966939+hayetyefsah@users.noreply.github.com>
2023-05-10 10:11:42 +0200

    test error main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
+#import os
 import json
-import feedparser
+#import feedparser
 from datetime import datetime
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
